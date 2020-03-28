0e36e213848a3ef43dc92655effe5a31b9c77a07
Priyam Harsh <priyamharsh14@gmail.com>
2020-03-28 19:51:02 +0530

    Updated Code

--- cbwebsite/quiz/views.py
+++ cbwebsite/quiz/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+	if request.method == "GET":
+		msg = "YEAH !! A user just opened your website"
+		print(msg)
+	elif request.method == "POST":
+		msg = "YEAH !! A user just sent some data to your website"
+		print(msg)
+	else:
+		msg = "not suppported method"
+		print(msg)
+	return HttpResponse(msg)
